1eca7a3adcf3b821543ac5f0354a97d4d7d093db
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-04-25 16:28:05 -0500

    add fix components

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,21 @@
 import NavBar from "./Navbar"
 import Inicio from "./Inicio";
-// import ParaTi from "./ParaTi";
-// import ComoFunciona from "./ComoFunciona";
-// import CTA from "./CTA";
-// import Footer from "./Footer";
-// import Testimonios from "./Testimonios";
+import ParaTi from "./ParaTi";
+import ComoFunciona from "./ComoFunciona";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Testimonios from "./Testimonios";
 
 const HomePage = () => {
     return (
     <>
         <NavBar/>
         <Inicio/>
-        {/* <ParaTi/> */}
-        {/* <Testimonios/> */}
-        {/* <ComoFunciona/> */}
-        {/* <CTA/> */}
-        {/* <Footer/> */}
+        <ParaTi/>
+        <Testimonios/>
+        <ComoFunciona/>
+        <CTA/>
+        <Footer/>
     </>
     );
 };
