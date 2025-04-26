8efd42f08e4aa08c06b946e1c162276009889768
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-04-25 20:50:45 -0500

    fix: resolve merge conflicts in CTA and HomePage components

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,11 @@
 import NavBar from "./Navbar"
 import Inicio from "./Inicio";
 import ParaTi from "./ParaTi";
-<<<<<<< HEAD
-import ComoFunciona from "./ComoFunciona";
-import CTA from "./CTA";
-import Footer from "./Footer";
 import Testimonios from "./Testimonios";
-=======
-import Testimonials from "./Testimonios";
 import ComoFunciona from "./ComoFunciona";
 import CTA from "./CTA";
 import Footer from "./Footer";
->>>>>>> 13b7f1822584107461c6dd1830be58b4f5c9b66c
+
 
 const HomePage = () => {
     return (
@@ -19,11 +13,7 @@ const HomePage = () => {
         <NavBar/>
         <Inicio/>
         <ParaTi/>
-<<<<<<< HEAD
         <Testimonios/>
-=======
-        <Testimonials/>
->>>>>>> 13b7f1822584107461c6dd1830be58b4f5c9b66c
         <ComoFunciona/>
         <CTA/>
         <Footer/>
